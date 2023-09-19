eb50c117d2eedc92c3526b13834a528f482723c3
vladimir <tarskij-vladimir@yandex.ru>
2023-09-19 23:22:57 +0300

    added treugolnik

---
+++ examples_007_Magic/Program.cs
@@ -0,0 +1,19 @@
+﻿//Console.Clear();
+//Console.SetCursorPosition(10, 4 );
+//Console.WriteLine("+");
+int xa = 1;
+int ya = 1;
+int xb = 1;
+int yb = 1;
+int xc = 1;
+int yc = 1;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
